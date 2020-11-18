10d29a7fcac22e9ec3d608533dd755b7bff5d7a3
Victor <victor.melocariati@gmail.com>
2020-11-18 16:16:36 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Usuario_e_senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+            string senha, nome; 
+
+            Console.WriteLine("Olá,\nSeja Bem-Vindo(a)!");
+            Console.WriteLine("  ");
+
+            Console.WriteLine("Insira seu nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Insira uma senha:");
+            senha = Console.ReadLine();
+
+            while(senha == nome){
+                Console.WriteLine("   ");
+                  Console.WriteLine("Insira uma senha válida:");
+                  senha = Console.ReadLine();
+            }          
+            Console.WriteLine("   ");
+            Console.WriteLine("Login Efetivado!");
+        }
+    }
+}
